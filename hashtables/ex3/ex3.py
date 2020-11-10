03e433f5ccb892df7c1231bafbd785631e89c7f8
Frytz <dacook1983@gmail.com>
2020-11-09 22:11:52 -0500

    ex3 completed

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,7 +3,19 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
-
+    #create dict
+    int_dict= {}
+    #create array
+    result = []
+    #loop through the array and add to the dict, increment in there is a dup
+    for i in arrays:
+        for j in i:
+            if j not in int_dict:
+                int_dict[j] = 1
+            else:
+                int_dict[j] += 1
+                if int_dict[j] == len(arrays):
+                    result.append(j)
     return result
 
 
