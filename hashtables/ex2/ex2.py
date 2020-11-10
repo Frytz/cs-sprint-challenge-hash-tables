59060c6d3a2518b538f941285081ae5e70fbd4f6
Frytz <dacook1983@gmail.com>
2020-11-09 21:41:39 -0500

    second ex done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    #define route length and location
+    route = [None] * length
+    tickets_dict = {}
+    #loop through tickets setting dest and next location
+    for ticket in tickets:
+        tickets_dict[ticket.source] = ticket.destination
+    next = tickets_dict["NONE"]
+
+    #loop over length setting the route index to the next location and the next to the next location in the list 
+    for i in range(0, length):
+        route[i] = next
+        next = tickets_dict[next]
+
 
     return route
