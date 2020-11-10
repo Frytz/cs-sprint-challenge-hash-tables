99567dcc1f70ad67854f96f723d550804610afc5
Frytz <dacook1983@gmail.com>
2020-11-09 22:43:13 -0500

    all completed quetions, answered

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,22 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+        # create a hashtable
+    hashtable = {}
+    # create a result array
+    result = []
+    # loop through and add ext as the key and an array of paths for the value
+    for i in files:
+        subStringArray = i.split("/")
+        ext = subStringArray[len(subStringArray) - 1]
+        if ext in hashtable:
+            hashtable[ext].append(i)
+        else:
+            hashtable[ext] = [i]
+    # loop through queries and add bating key values to the array(concat the arrays together)
+    for i in queries:
+        if i in hashtable:
+            result = result + hashtable[i]
 
     return result
 
